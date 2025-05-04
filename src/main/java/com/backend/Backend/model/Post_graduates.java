805abae83a61b67ca0f75c02743c2da8c8fe2fdb
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-04 21:29:15 +0530

    Added details to the post_graduate model

--- src/main/java/com/backend/Backend/model/Post_graduates.java
+++ src/main/java/com/backend/Backend/model/Post_graduates.java
@@ -1,4 +1,24 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Post_graduates {
+    @Id
+    private String id;
+    private String institute;
+    private String research_interest;
+    private String second_degree;
+
+    @ManyToMany
+    @JoinTable(name = "post_graduate_sessions",
+                joinColumns = @JoinColumn(name = "post_graduates_id"),
+                 inverseJoinColumns = @JoinColumn(name = "session_id"))
+
+    private List<Session> sessions;
+
+
+
 }
