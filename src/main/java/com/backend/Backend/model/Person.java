48ab4fc8e960822d1291f42772992d00cd34dd10
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-04 21:21:58 +0530

    Added details of the Person model

--- src/main/java/com/backend/Backend/model/Person.java
+++ src/main/java/com/backend/Backend/model/Person.java
@@ -4,5 +4,10 @@ import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
 public class Person {
+    private String email;
+    private String gender;
+    private String name;
+    private int phone;
+
 
 }
