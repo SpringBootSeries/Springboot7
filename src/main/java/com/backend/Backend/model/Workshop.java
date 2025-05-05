820bc88e88eedf51058721f71978a854350ba4f6
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 12:37:24 +0530

    Added details to the Workshop model

--- src/main/java/com/backend/Backend/model/Workshop.java
+++ src/main/java/com/backend/Backend/model/Workshop.java
@@ -1,4 +1,16 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
 public class Workshop {
+    @Id
+    private String id;
+    private String description;
+    private Date end_date;
+    private String name;
+    private Date start_date;
 }
