b7f3f7eae5057feb8e1fccef9be69071cd42683a
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 12:36:26 +0530

    Added details of the UnderGraduates model

--- src/main/java/com/backend/Backend/model/Under_graduates.java
+++ src/main/java/com/backend/Backend/model/Under_graduates.java
@@ -1,4 +1,19 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Under_graduates {
+    @Id
+    private String id;
+    private String degree;
+    private String university;
+
+    @ManyToMany
+    @JoinTable(name="under_graduate_sessions ",
+            joinColumns = @JoinColumn(name="under_graduates_id"),
+            inverseJoinColumns = @JoinColumn(name="sessions_id"))
+    private List<Session> sessions;
 }
