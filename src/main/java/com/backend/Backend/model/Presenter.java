07b7968593384f9973646ad8f08ea95589d59f33
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 11:39:17 +0530

    Added details of the Presenter model

--- src/main/java/com/backend/Backend/model/Presenter.java
+++ src/main/java/com/backend/Backend/model/Presenter.java
@@ -1,4 +1,24 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+
 public class Presenter {
+    @Id
+    private String id;
+    private String affiliation;
+    private String country;
+    private String ob_title;
+
+    @ManyToMany
+    @JoinTable(name = "presenter_sessions",
+                joinColumns = @JoinColumn(name = "presenter_id"),
+    inverseJoinColumns = @JoinColumn(name = "sessions_id"))
+
+    private List<Session>sessions;
+    g
+
 }
