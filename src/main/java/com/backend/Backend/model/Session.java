329e63c41aa97e1ebff37ae892f19d003b01c0b3
Dinuki85 <prarthanadinuki2@gmail.com>
2025-05-05 12:15:20 +0530

    Added details of the Session model

--- src/main/java/com/backend/Backend/model/Session.java
+++ src/main/java/com/backend/Backend/model/Session.java
@@ -1,4 +1,29 @@
 package com.backend.Backend.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+
+import java.sql.Time;
+import java.util.Date;
+import java.util.List;
+
+@Entity
 public class Session {
+    @Id
+    private String id;
+    private Date date;
+    private String description;
+    private String name;
+    private Time time;
+    private int workshop_id;
+
+    @ManyToMany(mappedBy="sessions")
+    private List<Post_graduates> post_graduates;
+
+    @ManyToMany(mappedBy="sessions")
+    private List<Under_graduates> under_graduates;
+
+    @ManyToMany(mappedBy="sessions")
+    private List<Presenter>presenters;
 }
